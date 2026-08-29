badf4392065c3feabb698408c4e82a3fb329c979
Noman <pxycknomdictator@outlook.com>
2026-08-30 02:17:17 +0500

    feat: add social providers in better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -18,5 +18,37 @@ export const auth = betterAuth({
         transaction: true,
     }),
     emailVerification: { expiresIn: 60 * 5 },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            mapProfileToUser: async (profile) => ({
+                name: profile.given_name,
+                email: profile.email,
+                emailVerified: profile.email_verified,
+                image: profile.picture,
+            }),
+        },
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+            mapProfileToUser: async (profile) => ({
+                name: profile.login,
+                email: profile.email,
+                emailVerified: Boolean(profile.email),
+                image: profile.avatar_url,
+            }),
+        },
+        discord: {
+            clientId: process.env.DISCORD_CLIENT_ID as string,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+            mapProfileToUser: async (profile) => ({
+                name: profile.global_name || profile.username,
+                email: profile.email,
+                emailVerified: profile.verified,
+                image: profile.image_url,
+            }),
+        },
+    },
     plugins: [nextCookies()],
 });
