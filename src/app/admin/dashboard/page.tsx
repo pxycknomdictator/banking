5d994bca13599715275e00c82f0e5eb7cbebb0b3
Noman <pxycknomdictator@outlook.com>
2026-09-03 15:40:18 +0500

    feat: add admin dashboard page

---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminDashboardPage() {
+    return (
+        <div className="p-4">
+            <h1 className="font-medium text-2xl">Admin dashboard page</h1>
+        </div>
+    );
+}
