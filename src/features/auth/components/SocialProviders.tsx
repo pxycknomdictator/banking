488971115b3f64424c545c0abf083b34cd9b7d68
Noman <pxycknomdictator@outlook.com>
2026-09-12 06:47:55 +0500

    feat: add last used badge in sign in form

--- src/features/auth/components/SocialProviders.tsx
+++ src/features/auth/components/SocialProviders.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
 export function SocialProviders() {
@@ -21,8 +22,14 @@ export function SocialProviders() {
                     key={id}
                     type="button"
                     variant={"outline"}
-                    className="py-7 space-x-1 disabled:opacity-45 disabled:cursor-not-allowed cursor-pointer"
+                    className="relative py-7 space-x-1 disabled:opacity-45 disabled:cursor-not-allowed cursor-pointer"
                 >
+                    <Badge
+                        variant="default"
+                        className="absolute -top-2.5 -right-3 font-semibold md:font-medium"
+                    >
+                        Last used
+                    </Badge>
                     <Image src={src} alt={id} width={25} height={25} />
                     <span className="block font-semibold lg:font-medium">
                         {text}
