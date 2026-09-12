638b1c018536593b25cc08cd133be652c33b1f54
Noman <pxycknomdictator@outlook.com>
2026-09-12 20:29:27 +0500

    feat: add forgot password form

--- src/features/auth/components/forms/ForgotPasswordForm.tsx
+++ src/features/auth/components/forms/ForgotPasswordForm.tsx
@@ -1,9 +1,41 @@
 "use client";
 
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { FormHeader } from "@/features/auth/components/FormHeader";
+import { FormWrapper } from "@/features/auth/components/FormWrapper";
+
 export function ForgotPasswordForm() {
     return (
-        <div className="p-4">
-            <h1 className="font-medium text-2xl">Forgot password form</h1>
-        </div>
+        <FormWrapper>
+            <CardHeader>
+                <FormHeader
+                    title="Forgot password form"
+                    description="Enter your credentials"
+                />
+            </CardHeader>
+            <CardContent></CardContent>
+            <CardFooter>
+                <div className="w-full space-y-5.5">
+                    <Button
+                        type="submit"
+                        variant={"default"}
+                        className="py-7 w-full font-semibold disabled:opacity-45 disabled:cursor-not-allowed cursor-pointer"
+                    >
+                        Reset password
+                    </Button>
+                    <div className="text-center font-medium md:font-normal">
+                        Remember your password?{" "}
+                        <Link
+                            className="hover:underline font-semibold lg:font-medium"
+                            href={"/sign-in"}
+                        >
+                            Sign in
+                        </Link>
+                    </div>
+                </div>
+            </CardFooter>
+        </FormWrapper>
     );
 }
