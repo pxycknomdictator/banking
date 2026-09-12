d209afa9e37d7e186c4f57418689f32c1a7820fd
Noman <pxycknomdictator@outlook.com>
2026-09-12 05:25:29 +0500

    feat: add form wrapper component

---
+++ src/features/auth/components/FormWrapper.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import { Card } from "@/components/ui/card";
+
+export type FormWrapperProps = {
+    children: ReactNode;
+};
+
+export function FormWrapper({ children }: FormWrapperProps) {
+    return <Card>{children}</Card>;
+}
--- src/features/auth/components/forms/SignupForm.tsx
+++ src/features/auth/components/forms/SignupForm.tsx
@@ -1,28 +1,26 @@
 "use client";
 
 import {
-    Card,
     CardContent,
     CardDescription,
     CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import { FormWrapper } from "@/features/auth/components/FormWrapper";
 import { SocialProviders } from "@/features/auth/components/SocialProviders";
 
 export function SignupForm() {
     return (
-        <div className="p-4">
-            <Card>
-                <CardHeader>
-                    <CardTitle>Sign up form</CardTitle>
-                    <CardDescription></CardDescription>
-                </CardHeader>
-                <CardContent></CardContent>
-                <CardFooter>
-                    <SocialProviders />
-                </CardFooter>
-            </Card>
-        </div>
+        <FormWrapper>
+            <CardHeader>
+                <CardTitle>Sign up form</CardTitle>
+                <CardDescription></CardDescription>
+            </CardHeader>
+            <CardContent></CardContent>
+            <CardFooter>
+                <SocialProviders />
+            </CardFooter>
+        </FormWrapper>
     );
 }
