89837b1f2454955d8beb3caff64a86a51fd5e629
Noman <pxycknomdictator@outlook.com>
2026-09-12 20:54:51 +0500

    feat: add otp form

--- src/features/auth/components/forms/OTPForm.tsx
+++ src/features/auth/components/forms/OTPForm.tsx
@@ -1,9 +1,41 @@
 "use client";
 
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { FormHeader } from "@/features/auth/components/FormHeader";
+import { FormWrapper } from "@/features/auth/components/FormWrapper";
+
 export function OTPForm() {
     return (
-        <div className="p-4">
-            <h1 className="font-medium text-2xl">OTP form</h1>
-        </div>
+        <FormWrapper>
+            <CardHeader>
+                <FormHeader
+                    title="OTP form"
+                    description="Enter your credentials"
+                />
+            </CardHeader>
+            <CardContent></CardContent>
+            <CardFooter>
+                <div className="w-full space-y-5.5">
+                    <Button
+                        type="submit"
+                        variant={"default"}
+                        className="py-7 w-full font-semibold disabled:opacity-45 disabled:cursor-not-allowed cursor-pointer"
+                    >
+                        Verify OTP
+                    </Button>
+                    <div className="text-center font-medium md:font-normal">
+                        Having trouble verifying?{" "}
+                        <Link
+                            className="hover:underline font-semibold lg:font-medium"
+                            href={"/two-factor/totp-verify"}
+                        >
+                            Authenticator app
+                        </Link>
+                    </div>
+                </div>
+            </CardFooter>
+        </FormWrapper>
     );
 }
