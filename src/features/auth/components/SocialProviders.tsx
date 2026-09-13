498a6a47fc84de69f64f23187b5428c6e8a1489c
Noman <pxycknomdictator@outlook.com>
2026-09-13 21:34:35 +0500

    feat: add social provider event handler

--- src/features/auth/components/SocialProviders.tsx
+++ src/features/auth/components/SocialProviders.tsx
@@ -8,8 +8,10 @@ import { Button } from "@/components/ui/button";
 export function SocialProviders() {
     const pathname = usePathname();
 
+    type SocialProvider = "google" | "github" | "discord";
+
     type SocialProviderList = {
-        id: "google" | "github" | "discord";
+        id: SocialProvider;
         text: "Google" | "Github" | "Discord";
         src: string;
     };
@@ -20,6 +22,10 @@ export function SocialProviders() {
         { id: "discord", text: "Discord", src: "/discord.svg" },
     ];
 
+    async function signInWithSocialProvider(provider: SocialProvider) {
+        console.log({ provider });
+    }
+
     return (
         <section className="w-full grid grid-cols-1 mb-2 gap-y-4 md:grid-cols-3 gap-x-4">
             {socialProviderList.map(({ id, text, src }) => (
@@ -27,6 +33,7 @@ export function SocialProviders() {
                     key={id}
                     type="button"
                     variant={"outline"}
+                    onClick={() => signInWithSocialProvider(id)}
                     className="relative py-7 space-x-1 disabled:opacity-45 disabled:cursor-not-allowed cursor-pointer"
                 >
                     {pathname === "/sign-in" && (
