36e1849c67955752d5f2fbefb0ee6f301865125b
Noman <pxycknomdictator@outlook.com>
2026-09-21 05:01:08 +0500

    feat: add social providers in better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -37,5 +37,19 @@ export const auth = betterAuth({
         client: redis,
         keyPrefix: "better-auth:"
     }),
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        },
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string
+        },
+        discord: {
+            clientId: process.env.DISCORD_CLIENT_ID as string,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET as string
+        }
+    },
     plugins: [nextCookies()]
 });
