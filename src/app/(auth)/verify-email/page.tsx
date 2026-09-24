aab76d8d22b66f2ddddfd6bee34c36e00127ac96
Noman <pxycknomdictator@outlook.com>
2026-09-25 03:12:06 +0500

    feat: add unverified dal for email verification check

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,4 +1,7 @@
-export default function VerifyEmail() {
+import { unverifiedSession } from "@/dal/auth";
+
+export default async function VerifyEmail() {
+    await unverifiedSession();
     return (
         <div className="p-4">
             <h1 className="text-2xl font-medium">Verify email page</h1>
--- src/dal/auth.ts
+++ src/dal/auth.ts
@@ -24,3 +24,12 @@ export async function adminSession(): Promise<SessionResponse> {
     if (session.user.role !== "admin") redirect("/dashboard");
     return session;
 }
+
+export async function unverifiedSession(): Promise<SessionResponse> {
+    const session = await getSession();
+    if (session.user.emailVerified) {
+        if (session.user.role !== "admin") redirect("/dashboard");
+        else redirect("/admin/dashboard");
+    }
+    return session;
+}
