239b04d5abf7af1e9acfaa69fa5b7b4c6781e706
Noman <pxycknomdictator@outlook.com>
2026-09-25 03:00:44 +0500

    feat: add user and admin dashboard routes

---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminDashboard() {
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-medium">Admin Dashboard page</h1>
+        </div>
+    );
+}
