85919a06a8e00024fa3120904ed8198a1793a036
Noman <pxycknomdictator@outlook.com>
2026-09-26 19:09:42 +0500

    feat: add password reset form

--- src/features/auth/components/forms/ResetPasswordForm.tsx
+++ src/features/auth/components/forms/ResetPasswordForm.tsx
@@ -1,11 +1,32 @@
 "use client";
 
+import Link from "next/link";
+import { UserRoundPen } from "lucide-react";
+import { CardFooter, CardHeader } from "@/components/ui/card";
+import { FormHeader } from "@/features/auth/components/FormHeader";
+import { FormRoundIcon } from "@/features/auth/components/FormRoundIcon";
 import { FormWrapper } from "@/features/auth/components/FormWrapper";
 
 export function ResetPasswordForm() {
     return (
         <FormWrapper>
-            <h1 className="text-2xl font-medium">Reset password form</h1>
+            <CardHeader>
+                <FormRoundIcon LucideIcon={UserRoundPen} />
+                <FormHeader title="Reset password form" description="" />
+            </CardHeader>
+            <CardFooter>
+                <section className="w-full">
+                    <div className="text-center my-4">
+                        Remember your password?{" "}
+                        <Link
+                            href={"/sign-in"}
+                            className="hover:underline font-medium"
+                        >
+                            Sign in
+                        </Link>
+                    </div>
+                </section>
+            </CardFooter>
         </FormWrapper>
     );
 }
