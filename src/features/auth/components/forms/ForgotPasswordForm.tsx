e6d40d247456ef57ad630be088a84fbe8d161264
Noman <pxycknomdictator@outlook.com>
2026-09-26 19:06:02 +0500

    feat: add forgot password form

--- src/features/auth/components/forms/ForgotPasswordForm.tsx
+++ src/features/auth/components/forms/ForgotPasswordForm.tsx
@@ -1,11 +1,32 @@
 "use client";
 
+import Link from "next/link";
+import { LucideKey } from "lucide-react";
+import { CardFooter, CardHeader } from "@/components/ui/card";
+import { FormHeader } from "@/features/auth/components/FormHeader";
+import { FormRoundIcon } from "@/features/auth/components/FormRoundIcon";
 import { FormWrapper } from "@/features/auth/components/FormWrapper";
 
 export function ForgotPasswordForm() {
     return (
         <FormWrapper>
-            <h1 className="text-2xl font-medium">Forgot password form</h1>
+            <CardHeader>
+                <FormRoundIcon LucideIcon={LucideKey} />
+                <FormHeader title="Forgot password form" description="" />
+            </CardHeader>
+            <CardFooter>
+                <section className="w-full">
+                    <div className="text-center my-4">
+                        Remember your password?{" "}
+                        <Link
+                            href={"/sign-in"}
+                            className="hover:underline font-medium"
+                        >
+                            Sign in
+                        </Link>
+                    </div>
+                </section>
+            </CardFooter>
         </FormWrapper>
     );
 }
